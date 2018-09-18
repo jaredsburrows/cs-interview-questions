bbebf395c52cd9e67cf913b240711b376585f6b3
Jared Burrows <jaredsburrows@gmail.com>
2018-09-17 17:21:24 -0700

    update spek 1.2.1

--- kotlin/src/test/kotlin/api/ColorSpec.kt
+++ kotlin/src/test/kotlin/api/ColorSpec.kt
@@ -2,23 +2,20 @@ package api
 
 import com.google.common.truth.Truth.assertThat
 import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.context
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
-import org.junit.platform.runner.JUnitPlatform
-import org.junit.runner.RunWith
 
-@RunWith(JUnitPlatform::class)
 object ColorSpec : Spek({
     describe("Color Enum") {
-        on("'valueOf' should handle colors") {
-            it("should") {
+        context("'valueOf' String") {
+            it("should 'valueOf' should handle colors") {
                 assertThat(Color.valueOf("BLUE")).isEqualTo(Color.BLUE)
             }
         }
 
-        on("'values' size should be") {
-            it("should") {
+        context("'values' size") {
+            it("should only have 3 colors") {
                 assertThat(Color.values().size).isEqualTo(3)
             }
         }
--- kotlin/src/test/kotlin/api/NodeSpec.kt
+++ kotlin/src/test/kotlin/api/NodeSpec.kt
@@ -2,24 +2,21 @@ package api
 
 import com.google.common.truth.Truth.assertThat
 import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.context
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
-import org.junit.platform.runner.JUnitPlatform
-import org.junit.runner.RunWith
 
-@RunWith(JUnitPlatform::class)
 object NodeSpec : Spek({
     describe("Node for answer coding questions") {
         val node = Node<Int>()
-        on("basic usage with no value") {
+        context("basic usage with no value") {
             node.value = 123
             it("should be 123") {
                 assertThat(node.value).isEqualTo(123)
             }
         }
 
-        on("basic usage with inital value") {
+        context("basic usage with initial value") {
             it("should be 123") {
                 assertThat(Color.values().size).isEqualTo(3)
             }
