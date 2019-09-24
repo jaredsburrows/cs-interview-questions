7cccf7340a0306e272beb5b3a01e22c3749b3b21
Jared Burrows <jaredsburrows@gmail.com>
2019-09-23 20:11:52 -0700

    clean up publish docs

--- .buildscript/publish-docs.sh
+++ .buildscript/publish-docs.sh
@@ -4,7 +4,6 @@ set -e
 
 SLUG="jaredsburrows/cs-interview-questions"
 BRANCH="master"
-DIR=temp-clone
 
 if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
     echo "Skipping: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
@@ -17,20 +16,21 @@ elif [ "$TRAVIS_OS_NAME" != "linux" ]; then
 else
     echo -e "Publishing docs...\n"
 
+    # Delete any existing temporary website clone
+    rm -rf temp-clone
+    rm -rf asciidoc-latest
+
     # Generate docs
     ./gradlew clean asciidoc
     cp -R build/asciidoc/html5/ asciidoc-latest/
 
-    # Delete any existing temporary website clone
-    rm -rf $DIR
-
     # Clone the current repo into temp folder
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "travis-ci"
-    git clone --quiet https://${GH_TOKEN}@github.com/${SLUG} $DIR &> /dev/null
+    git clone --quiet https://${GH_TOKEN}@github.com/${SLUG} temp-clone &> /dev/null
 
     # Move working directory into temp folder
-    cd $DIR
+    cd temp-clone
 
     # Checkout and track the gh-pages branch
     git checkout -t origin/gh-pages
@@ -50,7 +50,7 @@ else
     git push -fq origin gh-pages > /dev/null
 
     # Delete our temp folder
-    rm -rf ../$DIR
+    rm -rf ../temp-clone
 
     echo -e "Published docs to gh-pages.\n"
 fi
