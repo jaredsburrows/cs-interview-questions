ddac467ee3cfe54dc9018fa842afca67dee3d659
Jared Burrows <jaredsburrows@gmail.com>
2021-01-11 00:45:14 -0500

    move to github actions

--- .buildscript/publish-docs.sh
+++ .buildscript/publish-docs.sh
@@ -5,52 +5,40 @@ set -e
 SLUG="jaredsburrows/cs-interview-questions"
 BRANCH="master"
 
-if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
-    echo "Skipping: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
-elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    echo "Skipping: was pull request."
-elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
-    echo "Skipping: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
-elif [ "$TRAVIS_OS_NAME" != "linux" ]; then
-    echo "Skipping: wrong os. Expected 'osx' but was '$TRAVIS_OS_NAME'."
-else
-    echo -e "Publishing docs...\n"
+# Delete any existing temporary website clone
+rm -rf temp-clone
+rm -rf asciidoc-latest
 
-    # Delete any existing temporary website clone
-    rm -rf temp-clone
-    rm -rf asciidoc-latest
+# Generate docs
+./gradlew clean asciidoc
+cp -R build/asciidoc/html5/ asciidoc-latest/
 
-    # Generate docs
-    ./gradlew clean asciidoc
-    cp -R build/asciidoc/html5/ asciidoc-latest/
+# Clone the current repo into temp folder
+git config --global user.email "action@github.com"
+git config --global user.name "Github Actions"
+git clone --quiet https://${GH_TOKEN}@github.com/${SLUG} temp-clone &> /dev/null
 
-    # Clone the current repo into temp folder
-    git config --global user.email "travis@travis-ci.org"
-    git config --global user.name "travis-ci"
-    git clone --quiet https://${GH_TOKEN}@github.com/${SLUG} temp-clone &> /dev/null
+# Move working directory into temp folder
+cd temp-clone
 
-    # Move working directory into temp folder
-    cd temp-clone
+# Checkout and track the gh-pages branch
+git checkout -t origin/gh-pages
 
-    # Checkout and track the gh-pages branch
-    git checkout -t origin/gh-pages
+# Delete everything
+rm -rf *
 
-    # Delete everything
-    rm -rf *
+# Copy website files from real repo
+cp -R ../asciidoc-latest/* .
 
-    # Copy website files from real repo
-    cp -R ../asciidoc-latest/* .
+# Stage all files in git and create a commit
+git add .
+git add -u
+git commit -m "Website at $(date)"
 
-    # Stage all files in git and create a commit
-    git add .
-    git add -u
-    git commit -m "Website at $(date)"
+# Push the new files up to GitHub
+git push -fq origin gh-pages > /dev/null
 
-    # Push the new files up to GitHub
-    git push -fq origin gh-pages > /dev/null
+# Delete our temp folder
+rm -rf ../temp-clone
 
-    # Delete our temp folder
-    rm -rf ../temp-clone
-
-    echo -e "Published docs to gh-pages.\n"
-fi
+echo -e "Published docs to gh-pages.\n"
